0494b3fe324fd12ea3a933fcaa133da61fb3f2ba
unknown <davlatovamriddin7@gmail.com>
2023-04-19 15:39:39 +0500

    add tabs

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,41 @@
 const headerWrapper = document.querySelector('.header__wrapper')
 const burger = document.querySelector('.burger')
 
+// Before/After images effect
+function beforeAfter() {
+    document.getElementById('kobavenusab').style.width = document.getElementById('pedsumid').value + "%"
+}
+
+// Tabs
+function tab() {
+    let tabNav = document.querySelectorAll('.tabs-nav__item'),
+        tabContent = document.querySelectorAll('.tab'),
+        tabName
+
+    tabNav.forEach(item => {
+        item.addEventListener('click', selectTabNav)
+    })
+
+    function selectTabNav() {
+        tabNav.forEach(item => {
+            item.classList.remove('is-active')
+        })
+        this.classList.add('is-active')
+        tabName = this.getAttribute('data-tab-name')
+        selectTabContent(tabName)
+    }
+
+    function selectTabContent(tabName) {
+        tabContent.forEach(item => {
+            item.classList.contains(tabName) ? item.classList.add('is-active') : item.classList.remove('is-active')
+        })
+    }
+
+}
+
+tab()
+
+// Burger menu
 burger.addEventListener('click', (evt) => {
     const logo = document.querySelector('#logo-main')
     if (logo) {
